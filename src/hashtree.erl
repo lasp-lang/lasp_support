ad8f1db843ec561f9a92cc80a548fa25fc71f6d6
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2015-10-04 14:52:15 -0700

    Use Erlang 18 compatible library.

--- src/hashtree.erl
+++ src/hashtree.erl
@@ -520,8 +520,7 @@ new_segment_store(Opts, State) ->
     %% flushed to disk at once when under a heavy uniform load.
     WriteBufferMin = proplists:get_value(write_buffer_size_min, Config, DefaultWriteBufferMin),
     WriteBufferMax = proplists:get_value(write_buffer_size_max, Config, DefaultWriteBufferMax),
-    UniqueTime = time_compat:unique_integer([positive]),
-    {Offset, _} = random:uniform_s(1 + WriteBufferMax - WriteBufferMin, UniqueTime),
+    {Offset, _} = random:uniform_s(1 + WriteBufferMax - WriteBufferMin, time_compat:timestamp()),
     WriteBufferSize = WriteBufferMin + Offset,
     Config2 = orddict:store(write_buffer_size, WriteBufferSize, Config),
     Config3 = orddict:erase(write_buffer_size_min, Config2),
